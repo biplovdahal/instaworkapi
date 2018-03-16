73ff4cf4ce986e82b9a08626255471ba214ee827
biplovdahal <root@Biplovs-MacBook-Pro.local>
2018-03-15 22:15:46 -0700

    added filter capibility

--- instaworkapi/views.py
+++ instaworkapi/views.py
@@ -4,11 +4,27 @@ from rest_framework import status
 import requests
 
 
+
 class listings(APIView):
 	def get(self, request, *args, **kw):
-		city = request.GET.get('city', None)
-		response = requests.get('https://instawork.com/react/get_jobs?filters=&locality='+city+'&page=1').json()
-		return Response(response)
+		city = request.GET.get('city', '')
+		filters = request.GET.get('filters','')
+		filterDictionary = {
+			'assistant_floor_manager':'1',
+			'bartender':'2',
+			'counter_staff':'3',
+			'dishwasher':'4'
+		}
+		if city:
+			city=city
+		for key, value in filterDictionary.iteritems():
+			if filters == key:
+				filters = value
+		try:
+			response = requests.get('https://instawork.com/react/get_jobs?filters='+filters+'&locality='+city+'&page=1')
+			return Response(response)
+		except:
+			return('internal errror!')
 
 
 
