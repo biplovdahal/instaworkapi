59955ffca8195257c1a8f7929180927184337e72
biplovdahal <root@Biplovs-MacBook-Pro.local>
2018-03-15 19:00:03 -0700

    fixes

--- instaworkapi/views.py
+++ instaworkapi/views.py
@@ -6,8 +6,8 @@ import requests
 
 class listings(APIView):
 	def get(self, request, *args, **kw):
-		city = request.GET.get('arg1', None)
-		response = requests.get('https://instawork.com/react/get_jobs?filters=&locality=San-Francisco-CA&page=1').json()
+		city = request.GET.get('city', None)
+		response = requests.get('https://instawork.com/react/get_jobs?filters=&locality='+city+'&page=1').json()
 		return Response(response)
 
 
